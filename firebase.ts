6799c22924108029c0eb349ef668b736c3ce88fd
Gerwin Mateo <106371149+GerwinMateo@users.noreply.github.com>
2024-12-27 15:25:57 -0900

    Updated some configuration
    Fixed configuration

--- firebase.ts
+++ firebase.ts
@@ -9,8 +9,8 @@ dotenv.config();
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
